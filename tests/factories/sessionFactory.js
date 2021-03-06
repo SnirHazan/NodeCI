83213072f28c3357466fb69b7bd2c6f0824c890c
Snir Hazan <snirhazan30@gmail.com>
2021-03-06 10:40:59 +0200

    little refactor

--- tests/factories/sessionFactory.js
+++ tests/factories/sessionFactory.js
@@ -15,5 +15,5 @@ module.exports = ({ _id }) => {
 
     return {
         session, sig
-    }
+    };
 }
--- tests/factories/userFactory.js
+++ tests/factories/userFactory.js
@@ -1,6 +1,4 @@
 const mongoose = require('mongoose');
 const User = mongoose.model('User');
 
-module.exports = () => {
-    return new User({}).save();
-}
+module.exports = () => new User({}).save();
